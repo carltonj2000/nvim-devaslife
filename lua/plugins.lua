c00dd696bf2ad2e526edba26e9c04482c5303dc6
Carlton Joseph <carlton.joseph@gmail.com>
2022-11-04 20:54:02 -0700

    add webdev-icons

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -34,8 +34,11 @@ packer.startup(function(use)
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
 
+  use 'nvim-tree/nvim-web-devicons'
+
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use "nvim-telescope/telescope-file-browser.nvim"
+
 
 end)
