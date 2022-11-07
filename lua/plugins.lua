929eaddff4ed47b11ef3ad683fc03d9c214505ab
Carlton Joseph <carlton.joseph@gmail.com>
2022-11-07 10:01:04 -0800

    add prettier and gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,50 +1,54 @@
 local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
+if not status then
+	print("Packer is not installed")
+	return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use({
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  })
-  use 'nvim-lualine/lualine.nvim'
+	use("wbthomason/packer.nvim")
+	use({
+		"svrana/neosolarized.nvim",
+		requires = { "tjdevries/colorbuddy.nvim" },
+	})
+	use("nvim-lualine/lualine.nvim")
 
-  use 'neovim/nvim-lspconfig'
+	use("neovim/nvim-lspconfig")
 
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
 
-  use 'onsails/lspkind.nvim' -- vscode-like pictograms
+	use("onsails/lspkind.nvim") -- vscode-like pictograms
 
-  use 'L3MON4D3/LuaSnip'
+	use("L3MON4D3/LuaSnip")
 
-  use({
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  })
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
 
-  use 'nvim-tree/nvim-web-devicons'
+	use("nvim-tree/nvim-web-devicons")
 
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use "nvim-telescope/telescope-file-browser.nvim"
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
 
-  use "akinsho/bufferline.nvim"
-  use "norcalli/nvim-colorizer.lua"
+	use("akinsho/bufferline.nvim")
+	use("norcalli/nvim-colorizer.lua")
 
-  use "glepnir/lspsaga.nvim"
+	use("glepnir/lspsaga.nvim")
 
-  use('jose-elias-alvarez/null-ls.nvim')
-  use('MunifTanjim/prettier.nvim')
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("MunifTanjim/prettier.nvim")
+
+	use("lewis6991/gitsigns.nvim")
 end)
